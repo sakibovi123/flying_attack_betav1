919e9c6a12ffe7df64cccdd3899ab4692c7c2d34
unknown <sakibovi123@gmail.com>
2021-11-05 03:04:25 +0600

    Obstacles Starting

---
+++ Scripts/bird.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class bird : MonoBehaviour
+{
+    Vector3 _initialPosition;
+    private bool _birdLaunched;
+    private float _timesittingAround;
+
+    [SerializeField] private float _launchPower = 500;
+    
+
+    private void Awake()
+    {
+        _initialPosition = transform.position;
+    }
+
+    private void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color = Color.blue;
+
+        Vector2 directionToInitialPosition = _initialPosition - transform.position ;
+        GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * _launchPower);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+        _birdLaunched = true;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(pos.x, pos.y);
+    }
+    // This method uses: It will be used 100 per second. When the game will start and the birds will be thrown out of the map it will
+    // reset the game and gave the player another chance to finish the level
+    private void Update()
+    {
+        // If the bird is idle this method will be pushed
+
+        if (_birdLaunched && GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1)
+        {
+            _timesittingAround += Time.deltaTime;
+        }
+
+
+        if (transform.position.y > 10 || transform.position.y < -10 || transform.position.x > 10 || transform.position.x < -20 || _timesittingAround > 3)
+        {
+            string resetScene = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(resetScene);
+        }
+
+
+    }
+}
